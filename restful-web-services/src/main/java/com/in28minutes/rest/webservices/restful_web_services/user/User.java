6df898c5aa5dc723796be2082e3bc6d39ed03a3f
cjm2681 <cjm2681@naver.com>
2024-08-29 16:55:57 +0900

    restapi,URI 버전관리,API정적,동적 필터링,

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
@@ -2,6 +2,8 @@ package com.in28minutes.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -10,9 +12,11 @@ public class User {
 	private Integer id;
 	
 	@Size(min=2, message = "최소 2글자를 입력해주세요")
+	@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message = "생일은 과거의 날짜여야 합니다")
+	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
