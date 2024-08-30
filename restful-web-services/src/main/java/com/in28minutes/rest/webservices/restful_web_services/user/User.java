3a1fa8a146ed0435c7aeae2509a067d44f1ff6e8
cjm2681 <cjm2681@naver.com>
2024-08-30 17:27:52 +0900

    restapi,URI 버전관리,API정적,동적 필터링,게시물생성,mysql연동까지

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
@@ -1,23 +1,42 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 public class User {
 	
+	public User() {
+		
+	}
+	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
+	
 	@Size(min=2, message = "최소 2글자를 입력해주세요")
-	@JsonProperty("user_name")
+	//@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message = "생일은 과거의 날짜여야 합니다")
-	@JsonProperty("birth_date")
+	//@JsonProperty("birth_date")
 	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> posts;
+	
+	
 	
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
@@ -54,6 +73,18 @@ public class User {
 
 	
 	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
+
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
